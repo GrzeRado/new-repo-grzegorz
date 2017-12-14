7deebe62063c60c7c4e3101c12dfc14fd320ef3a
Grzegorz <rc36@wp.pl>
2017-12-14 12:20:58 +0100

    taks from lesson

---
+++ JQERRY/js.js
@@ -0,0 +1,7 @@
+function toggle(){
+  $('div')
+      .slideUp(2000)
+      .delay(1000)
+      .slideDown(2000)
+
+    }
---
+++ JQERRY/task1/js.js
@@ -0,0 +1,6 @@
+var bodyEls = document.querySelectorAll('body *')
+
+for (var i = 0; i < bodyEls.length; i++)
+    bodyEls[i].className = 'red'
+
+$('body *').addClass('green')
