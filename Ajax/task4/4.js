f69748d984bb89aa6fc596097464d04fa1f21e97
Grzegorz <rc36@wp.pl>
2017-12-18 13:31:22 +0100

    Fetch

--- Ajax/task4/4.js
+++ Ajax/task4/4.js
@@ -6,4 +6,15 @@ fetch ('https://randomuser.me/api')
         console.log (data.results[0].name.last)
         console.log (data.results[0].picture.large)
         console.log (data.results[0].phone)
+
+        var a = data.results[0].name.last
+        var b = data.results[0].picture.large
+        var c = data.results[0].phone
+
+        var block = document.createElement('div')
+        document.body.appendChild(block)
+
+        block.innerHTML = '<p>' + a + '</p><p><img src ="' + b + '"></p><p>' + c + '</p>'
+
     })
+    // koniec
